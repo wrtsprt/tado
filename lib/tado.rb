30a94ad999d4a5dba8ebed617cb5643b02e5c786
Raphael Doehring <raphael.doehring@vasco.com>
2015-10-07 21:19:13 +0200

    Add version to be set in initialize.

--- lib/tado.rb
+++ lib/tado.rb
@@ -3,12 +3,13 @@ require 'json'
 
 class Tado
   include HTTParty
-  base_uri 'https://my.tado.com/mobile/1.4'
 
-  def initialize(username, password)
+  def initialize(username, password, api_version)
     @username = username
     @password = password
     @default_options = { query: { username: @username, password: @password } }
+
+    self.class.base_uri "https://my.tado.com/mobile/#{api_version}"
   end
 
   def getCurrentState
